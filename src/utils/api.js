89e7793c4be3b8173e7cbf1ef750595c2c36a7f4
Grant Cawley <grantlcawley@gmail.com>
2021-12-18 18:43:00 -0500

    player info

--- src/utils/api.js
+++ src/utils/api.js
@@ -13,13 +13,25 @@ const api = {
       url: `https://mlb-data.p.rapidapi.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='${name_part}'`,
       headers: { 
         'x-rapidapi-host': 'mlb-data.p.rapidapi.com', 
-        'x-rapidapi-key': '', 
+        'x-rapidapi-key': '57e5dce244msh89bb79ca2ceb6d6p1f5460jsn8aa022ab1847',
         'Content-Type': 'application/json'
       }
     };
-    
+   return axios(config);
+  },
+  playerInfo: function (id) {
+    var config = {
+      method: 'get',
+      url: `https://mlb-data.p.rapidapi.com/json/named.player_info.bam?sport_code='mlb'&player_id=${id}`,
+      headers: { 
+        'x-rapidapi-host': 'mlb-data.p.rapidapi.com', 
+        'x-rapidapi-key': '57e5dce244msh89bb79ca2ceb6d6p1f5460jsn8aa022ab1847',
+        'Content-Type': 'application/json'
+      }
+    };
    return axios(config);
   }
+
 };
 
 export default api
