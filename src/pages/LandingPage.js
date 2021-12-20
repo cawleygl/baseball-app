ea8f9a449815790494aeb458edc8249c94bd2235
Grant Cawley <grantlcawley@gmail.com>
2021-12-19 22:29:38 -0600

    Added player select

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,32 +1,41 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button, TextField, Grid, ButtonGroup } from '@mui/material';
 import api from '../utils/api';
+import Card from '../components/Card';
 
 function LandingPage() {
 
   const [player, setPlayer] = useState('');
-  const [info, setInfo] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [selectedPlayer, setSelectedPlayer] = useState('');
 
-  const searchAPI = player => {
+  useEffect(() => {
+    console.log("SelectedPlayer:", selectedPlayer);
+  }, [selectedPlayer]);
+
+  const handlePlayerSearch = player => {
     api.playerSearch(player)
       .then(res => {
         const { queryResults } = res.data.search_player_all
-        console.log("queryResults", queryResults);
+        console.log("Search Results:", queryResults);
         console.log("totalSize", queryResults.totalSize);
         if (queryResults.totalSize === "1") {
-          setInfo([queryResults.row]);
+          setSearchResults([queryResults.row]);
         } else {
-          setInfo(queryResults.row);
+          setSearchResults(queryResults.row);
         }
       })
       .catch(err => console.log(err));
   };
 
-  const handlePlayerSelect = player => {
-    console.log(info);
+  const handlePlayerSelect = playerId => {
+    api.playerInfo(playerId)
+      .then(res => {
+        const { player_info: { queryResults } } = res.data
+        setSelectedPlayer(queryResults.row);
+      })
+      .catch(err => console.log(err));
   };
-
-
 
   return (
     <Grid container spacing={2} sx={{ mt: 2 }} >
@@ -37,21 +46,21 @@ function LandingPage() {
         }} />
       </Grid>
       <Grid item xs={6}>
-        <Button variant="contained" onClick={() => { searchAPI(player); }}>Search</Button>
+        <Button variant="contained" onClick={() => { handlePlayerSearch(player); }}>Search</Button>
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={4}>
         <ButtonGroup
           orientation="vertical"
           aria-label="vertical contained button group"
           variant="text"
         >
 
-          {info.map(player => {
+          {searchResults.map(player => {
             return (
-              <Button fullwidth="true" key={player.player_id} value={player.player_id}  onClick={event => {
-                console.log(event.target.value);
-                }}>
-                {player.name_display_first_last}: {player.team_abbrev} {player.position}
+              <Button fullwidth="true" key={player.player_id} value={player.player_id} onClick={event => {
+                handlePlayerSelect(event.target.value);
+              }}>
+                {player.name_display_first_last} - {player.team_abbrev} {player.position}
               </Button>
 
             );
@@ -60,9 +69,12 @@ function LandingPage() {
         </ButtonGroup>
 
       </Grid>
+      <Grid item xs={8}>
+        <Card {...selectedPlayer} />
+      </Grid>
+
     </Grid>
   )
 }
-
 
 export default LandingPage;
