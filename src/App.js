b2510045864eea2bee6e1ff5981a01ca7ae081b6
Grant Cawley <grantlcawley@gmail.com>
2022-01-01 23:07:26 -0500

    Moved team data to utils

--- src/App.js
+++ src/App.js
@@ -1,144 +1,18 @@
 import * as React from 'react';
 import ButtonAppBar from './components/ButtonAppBar';
-import LandingPage from './pages/LandingPage';
+import PlayerSearch from './pages/PlayerSearch';
+import TeamRosters from './pages/TeamRosters';
+
 import { Container } from '@mui/material';
 import './App.css';
-import { createTheme, ThemeProvider } from '@material-ui/core';
 
 function App() {
-  const theme = createTheme({
-    palette: {
-      az: {
-        primary: '#A71930',
-        secondary: '#E3D4AD',
-      },
-      atl: {
-        primary: '#CE1141',
-        secondary: '#13274F',
-      },
-      bal: {
-        primary: '#DF4601',
-        secondary: '#000000',
-      },
-      bos: {
-        primary: '#BD3039',
-        secondary: '#0C2340',
-      },
-      chc: {
-        primary: '#0E3386',
-        secondary: '#CC3433',
-      },
-      cws: {
-        primary: '#27251F',
-        secondary: '#C4CED4',
-      },
-      cin: {
-        primary: '#C6011F',
-        secondary: '#000000',
-      },
-      cle: {
-        primary: '#0C2340',
-        secondary: '#E31937',
-      },
-      col: {
-        primary: '#33006F',
-        secondary: '#C4CED4',
-      },
-      det: {
-        primary: '#0C2340',
-        secondary: '#FA4616',
-      },
-      hou: {
-        primary: '#002D62',
-        secondary: '#EB6E1F',
-      },
-      kc: {
-        primary: '#004687',
-        secondary: '#BD9B60',
-      },
-      laa: {
-        primary: '#003263',
-        secondary: '#BA0021',
-      },
-      lad: {
-        primary: '#005A9C',
-        secondary: '#A5ACAF',
-      },
-      mia: {
-        primary: '#00A3E0',
-        secondary: '#000000',
-      },
-      mil: {
-        primary: '#FFC52F',
-        secondary: '#12284B',
-      },
-      min: {
-        primary: '#002B5C',
-        secondary: '#D31145',
-      },
-      nym: {
-        primary: '#002D72',
-        secondary: '#FF5910',
-      },
-      nyy: {
-        primary: '#0C2340',
-        secondary: '#C4CED3',
-      },
-      oak: {
-        primary: '#003831',
-        secondary: '#EFB21E',
-      },
-      phi: {
-        primary: '#E81828',
-        secondary: '#002D72',
-      },
-      pit: {
-        primary: '#27251F',
-        secondary: '#FDB827',
-      },
-      stl: {
-        primary: '#C41E3A',
-        secondary: '#0C2340',
-      },
-      sd: {
-        primary: '#2F241D',
-        secondary: '#FFC425',
-      },
-      sf: {
-        primary: '#FD5A1E',
-        secondary: '#27251F',
-      },
-      sea: {
-        primary: '#0C2C56',
-        secondary: '#005C5C',
-      },
-      tb: {
-        primary: '#092C5C',
-        secondary: '#8FBCE6',
-      },
-      tx: {
-        primary: '#003278',
-        secondary: '#C0111F',
-      },
-      tor: {
-        primary: '#134A8E',
-        secondary: '#1D2D5C',
-      },
-      was: {
-        primary: '#AB0003',
-        secondary: '#14225A',
-      }
-    }
-  });
-
 return (
   <div className="App">
-    <ThemeProvider theme={theme}>
       <ButtonAppBar />
       <Container maxWidth="sm">
-        <LandingPage />
+        <TeamRosters />
       </Container>
-    </ThemeProvider>
 
   </div>
 );
--- src/components/Card.js
+++ src/components/Card.js
@@ -9,6 +9,7 @@ import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
 import { styled } from '@mui/material/styles';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import teamData from '../utils/teamData';
 
 
 const BasicCard = (props) => {
@@ -21,128 +22,6 @@ const BasicCard = (props) => {
     setExpanded(!expanded);
   };
 
-  const teams = {
-    109: {
-      primary: '#A71930',
-      secondary: '#E3D4AD',
-    },
-    144: {
-      primary: '#CE1141',
-      secondary: '#13274F',
-    },
-    110: {
-      primary: '#DF4601',
-      secondary: '#000000',
-    },
-    111: {
-      primary: '#BD3039',
-      secondary: '#0C2340',
-    },
-    112: {
-      primary: '#0E3386',
-      secondary: '#CC3433',
-    },
-    145: {
-      primary: '#27251F',
-      secondary: '#C4CED4',
-    },
-    113: {
-      primary: '#C6011F',
-      secondary: '#000000',
-    },
-    114: {
-      primary: '#0C2340',
-      secondary: '#E31937',
-    },
-    115: {
-      primary: '#33006F',
-      secondary: '#C4CED4',
-    },
-    116: {
-      primary: '#0C2340',
-      secondary: '#FA4616',
-    },
-    117: {
-      primary: '#002D62',
-      secondary: '#EB6E1F',
-    },
-    118: {
-      primary: '#004687',
-      secondary: '#BD9B60',
-    },
-    108: {
-      primary: '#003263',
-      secondary: '#BA0021',
-    },
-    119: {
-      primary: '#005A9C',
-      secondary: '#A5ACAF',
-    },
-    146: {
-      primary: '#00A3E0',
-      secondary: '#000000',
-    },
-    158: {
-      primary: '#FFC52F',
-      secondary: '#12284B',
-    },
-    142: {
-      primary: '#002B5C',
-      secondary: '#D31145',
-    },
-    121: {
-      primary: '#002D72',
-      secondary: '#FF5910',
-    },
-    147: {
-      primary: '#0C2340',
-      secondary: '#C4CED3',
-    },
-    133: {
-      primary: '#003831',
-      secondary: '#EFB21E',
-    },
-    143: {
-      primary: '#E81828',
-      secondary: '#002D72',
-    },
-    134: {
-      primary: '#27251F',
-      secondary: '#FDB827',
-    },
-    138: {
-      primary: '#C41E3A',
-      secondary: '#0C2340',
-    },
-    135: {
-      primary: '#2F241D',
-      secondary: '#FFC425',
-    },
-    137: {
-      primary: '#FD5A1E',
-      secondary: '#27251F',
-    },
-    136: {
-      primary: '#0C2C56',
-      secondary: '#005C5C',
-    },
-    139: {
-      primary: '#092C5C',
-      secondary: '#8FBCE6',
-    },
-    140: {
-      primary: '#003278',
-      secondary: '#C0111F',
-    },
-    141: {
-      primary: '#134A8E',
-      secondary: '#1D2D5C',
-    },
-    120: {
-      primary: '#AB0003',
-      secondary: '#14225A',
-    }
-  };
   const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
     return <IconButton {...other} />;
@@ -157,10 +36,9 @@ const BasicCard = (props) => {
 
   React.useEffect(() => {
     console.log("Card Props:", props);
-    setPrimaryColor(teams[props.team_id].primary);
-    setSecondaryColor(teams[props.team_id].secondary);
-    let handle = 'http://www.twitter.com/' + props.twitter_id
-    setTwitterHandle(handle);
+    setPrimaryColor(teamData.mlbTeams.find(e => e.teamId = props.team_id).primary);
+    setSecondaryColor(teamData.find(e => e.teamId = props.team_id).secondary);
+    setTwitterHandle('http://www.twitter.com/' + props.twitter_id);
   }, [props]);
 
   return (
